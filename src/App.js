2d4aadd9fc61021112a2cc364acea475c867ddac
Jens Andersson <jensandersson112@gmail.com>
2020-05-27 12:57:27 +0200

    updated route paths

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 /* thired part packages */
 
-import { Switch, Route, HashRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 
 import { isLoaded, firebaseConnect, firestoreConnect } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 
 /* our components */
 import './styles/css/main.css';
@@ -12,11 +14,11 @@ import './styles/css/home.css';
 
 import Landing from './views/landing';
 import Home from './views/home';
+import Admin from './views/admin';
+import Chat from './views/chat';
+import Settings from './views/settings';
 import Categories_list from './views/categoriesList';
 import Lecture from './views/lecture';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import Admin from './views/admin';
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +29,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log('prop: ', this.props);
     this.props.firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({ user });
@@ -44,9 +45,10 @@ class App extends Component {
         {isLoaded(profile) && (
           <HashRouter>
             <Route path="/" exact component={profile.email ? Home : Landing} />
-            <Route path="/hem" component={profile.email ? Home : Landing} />
-            <Route path="/kategori_list" component={profile.email ? Categories_list : Landing} />
-            <Route path="/forlasning" component={profile.email ? Lecture : Landing} />
+            <Route path="/chat" component={profile.email ? Chat : Landing} />
+            <Route path="/settings" component={profile.email ? Settings : Landing} />
+            <Route path="/category_list" component={profile.email ? Categories_list : Landing} />
+            <Route path="/lecture" component={profile.email ? Lecture : Landing} />
             <Route path="/admin" component={profile.email ? Admin : Landing} />
           </HashRouter>
         )}
