1b816326864d5f7294e5a9c0e942d6ad7614bd5b
Ghlawnji Basheer <Basheer.Ghlawnji@vaxjo.se>
2020-06-04 11:16:26 +0200

    handle login & signup err

--- src/views/admin.jsx
+++ src/views/admin.jsx
@@ -21,38 +21,70 @@ class Admin extends Component {
       email: '',
       phoneNumber: '',
       role: '',
-      password: ''
+      password: '',
+      msg: '',
     };
     this.createUser = this.createUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.errorHandle = this.errorHandle.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this)
   }
 
 
 
   createUser(e) {
     e.preventDefault();
-    const profile = {
-      email: this.state.email,
-      phoneNumber: this.state.phoneNumber,
-      name: this.state.name,
-      role: 'student'
-    };
-    this.props.firebase.createUser(this.state, profile).catch(err => {
-      console.log('doCreateUserWithEmailAndPassword - err: ', err);
-    });
+    const {email, password} = this.state
+    if (email.length > 0 && password.length > 0) {
+      const profile = {
+        email: this.state.email,
+        phoneNumber: this.state.phoneNumber,
+        name: this.state.name,
+        role: this.state.role
+      };
+      this.props.firebase.createUser(this.state, profile).catch(err => {
+        console.log('doCreateUserWithEmailAndPassword - err: ', err);
+        this.errorHandle(err.code)
+      });
+    }
+    else {
+      this.setState({ msg: 'E-post och lösenord måste deklareras' });
+    }
+
   }
 
+
+
+
+  errorHandle(err) {
+    if (err === 'auth/wrong-password') {
+      this.setState({ msg: 'E-postadressen eller lösenord felaktig' });
+    }
+    if (err === 'auth/invalid-email') {
+      this.setState({ msg: 'E-postadressen är inte glitlig' });
+    }
+    if (err === 'auth/user-not-found') {
+      this.setState({ msg: 'E-postadressen finns inte' });
+    }
+    if (err === 'auth/email-already-in-use') {
+      this.setState({ msg: 'E-postadressen används redan av ett annat konto.' });
+    }
+    if (err === 'auth/weak-password') {
+      this.setState({ msg: 'Lösenordet måste vara minst 6 tecken' });
+    }
+  }
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  handleSelectChange = (event) => {
+    this.setState({
+      role: event.target.value
+    })
+  }
+
   render() {
     const { profile, lectures, users} = this.props;
-
-    console.log(typeof lectures);
-    
-
-
 
     return (
       <div className="container admin">
@@ -65,13 +97,19 @@ class Admin extends Component {
                 <i className="fa fa-cog fa-2x" aria-hidden="true"></i>
               </li>
             </ul>
+  
           </nav>
+          <div className="w-75 mx-auto">
+            <p className="bg-warning text-center" style={{ fontSize: '13px' }}>
+              {this.state.msg}
+            </p>
+          </div>
 
           <div className="edit">
             <ul>
               <li>
                 <button className="btn btn-primary " data-toggle="modal" data-target="#adduser">
-                  <i className="fa fa-plus fa-lg mr-2" aria-hidden="true"></i> Elev
+                  <i className="fa fa-plus fa-lg mr-2" aria-hidden="true"></i> Konto
                 </button>
               </li>
               <li>
@@ -138,9 +176,9 @@ class Admin extends Component {
                     </div>
                     <div className="form-group">
                       <label> Roll </label>
-                      <select className="form-control form-control-sm">
-                        <option name="role">elev</option>
-                        <option name="role">Lärare</option>
+                      <select className="form-control form-control-sm" onClick={this.handleSelectChange}>
+                        <option value="student" >elev</option>
+                        <option value="teacher">Lärare</option>
                       </select>
                     </div>
                     <div className="modal-footer">
