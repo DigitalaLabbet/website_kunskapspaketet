3ea97baeb87d4f90d3ce41fa93aab1c6e2c0ebb9
Michael Wagnberg <michael.wagnberg@dizparc.se>
2020-06-12 11:46:14 +0200

    Added accordion

--- src/views/lecture.jsx
+++ src/views/lecture.jsx
@@ -7,6 +7,7 @@ import { compose } from 'redux';
 import Topbar from '../components/topbar';
 import Navbar from '../components/navbar';
 import QuizModal from '../components/quiz-modal';
+import { Accordion, Card, Button } from 'react-bootstrap';
 
 class Lecture extends Component {
   render() {
@@ -21,24 +22,32 @@ class Lecture extends Component {
         {lecture && (
           <>
             <Topbar name={lecture.name} backLink="/" color={lecture.color} />
-            <div className="container navbar-margin">
-              <div className="lecture mt-2">
-                <iframe title="vidoe" width="100%" height="200px" src={lecture.videoUrl}></iframe>
-                <p className="text-14" dangerouslySetInnerHTML={innerHtml(lecture.information)}></p>
-                {lecture.links &&
-                  lecture.links.map(link => (
-                    <div className="text-14" key={link.heading}>
-                      {link.heading && <b>{link.heading}</b>}
-                      <ul>
-                        {link.items.map(item => (
-                          <li key={item} dangerouslySetInnerHTML={innerHtml(item)}></li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                {quizzes && quizzes.map(quiz => <QuizModal key={quiz.quizId} quiz={quiz} lectureId={lecture.id} />)}
+            <Accordion>
+              <div className="container navbar-margin">
+                <div className="lecture mt-2">
+                  <iframe title="vidoe" width="100%" height="200px" src={lecture.videoUrl}></iframe>
+                  <p className="text-14" dangerouslySetInnerHTML={innerHtml(lecture.information)}></p>
+                  {lecture.links &&
+                    lecture.links.map((link, index) => (
+                      <Card key={index}>
+                        <Card.Header className="py-1 px-2">
+                          <Accordion.Toggle as={Button} variant="link" eventKey={index} className="w-100 text-left">
+                            {link.heading}
+                          </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey={index}>
+                          <Card.Body className="px-3 py-2">
+                          {link.items.map(item => (
+                            <li key={item} dangerouslySetInnerHTML={innerHtml(item)}></li>
+                          ))}
+                          </Card.Body>
+                        </Accordion.Collapse>
+                      </Card>
+                    ))}
+                  {quizzes && quizzes.map(quiz => <QuizModal key={quiz.quizId} quiz={quiz} lectureId={lecture.id} />)}
+                </div>
               </div>
-            </div>
+            </Accordion>
           </>
         )}
         <Navbar role={profile.role} />
